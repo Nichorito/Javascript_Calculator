c4c5ffd58dff34857fcfb6a4ecd99cfa1769a357
Nicholas <nsheppard999@gmail.com>
2024-01-21 21:20:15 -0400

    Create reactive display
    Modified the mousedown event.
    
    Added an if else loop to check whether the button pressed are numbers or
    mathmatical operators.
    
    If they are numbers it runs a concatement function and updates the display.
    
    If it is a mathmatical operator then the operator should be stored (not currently
    functional) and sum1 is stored as a number a, then the initial loop runs
    again to begin incrementing sum 2 for use as b
    
    //////////////////////// Minor tweaks /////////////////////////
    
    Added a query selector and variable for each button
    
    added variables a => sum1, b => sum2, and operator.  Which will be used
    to control which function runs at which time, aswell as be used for
    function calls upon the equal press

--- script.js
+++ script.js
@@ -1,11 +1,77 @@
 let gridContainer = document.querySelector('#grid-container')
 let buttons = document.querySelectorAll(".grid-item");
+
 let addButton = document.querySelector('#add');
 let subtractButton = document.querySelector('#subtract');
 let multiplyButton = document.querySelector('#multiply');
 let divideButton = document.querySelector('#divide');
- let equalButton = document.querySelector('equals')
- 
+let equalButton = document.querySelector('equals')
+
+let zeroButton = document.querySelector('#zero');
+let oneButton = document.querySelector('#one');
+let twoButton = document.querySelector('#two');
+let threeButton = document.querySelector('#three');
+let fourButton = document.querySelector('#four')
+let fiveButton = document.querySelector('#five');
+let sixButton = document.querySelector('#six');
+let sevenButton = document.querySelector('#seven');
+let eightButton = document.querySelector('#eight');
+let nineButton = document.querySelector('#nine')
+
+let a = undefined;
+let sum1 = '';
+let b = undefined;
+let sum2 = '';
+let operator = '';
+//Store the value for a
+
+gridContainer.addEventListener('mousedown', (event) => {
+
+    //Checks whether the target is a button
+    if (event.target.classList.contains("grid-item")) {
+        console.log(`You clicked a button! It was the ${event.target.textContent} button`)
+
+        let selectedButton = event.target.textContent;
+
+        //Checks whether the button pressed is between 0-9.
+        //Regex eplanation:
+        // / & /  starts and ends the regex
+        // ^ &  $ begins and ends the string to test for
+        // [0-9] is the string to test for 
+
+        if (/^[0-9]$/.test(event.target.textContent)) {
+
+            console.log(`you pressed the ${selectedButton} button`)
+
+            //Checks whether a is currently exists, if it doesn't then it adds it to
+            //the displayed string
+            if (a == undefined ){
+                sum1 += selectedButton;
+                display.textContent = sum1;
+                console.log(sum1)
+            }
+            //If a currently exists, then start creating sum 2 
+            else {
+                sum2 += selectedButton;
+                console.log(sum2)
+            }
+            
+        }
+        else if (/^[+\-X÷]$/.test(event.target.textContent)) {
+            a = parseInt(sum1);
+        }
+        
+
+        //Depress button on mousedown
+        event.target.style.border = 'none';
+    }
+});
+
+
+
+
+
+
 const add = function(a, b) {
 	return a + b;
 };
@@ -19,21 +85,8 @@ const multiply = function(values) {
 };
 
 
-
-
-
-//Depress button on mousedown
-gridContainer.addEventListener('mousedown', (event) => {
-    console.log("You clicked a button!")
-
-    if (event.target.classList.contains("grid-item")) {
-        event.target.style.border = 'none';
-    }
-})
-
 //Release button on mouseup
 gridContainer.addEventListener('mouseup', (event) => {
-    console.log("You clicked a button!")
 
     if (event.target.classList.contains("grid-item")) {
         event.target.style.borderBottom = '3px solid #24246B';  
